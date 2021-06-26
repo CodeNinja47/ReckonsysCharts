5ab18ea99218371d297b4f45882b3c064167d6e1
CodeNinja47 <dinesh@reckonsys.com>
2021-06-26 10:49:25 +0530

    Add: HalfDonutChart

--- src/index.js
+++ src/index.js
@@ -1,42 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles.module.css'
 
 import PieChart from './components/Charts/PieChart';
 import DonutChart from './components/Charts/DonutChart';
+import HalfDonutChart from './components/HalfDonutChart/HalfDonutChart';
+
+const options = {
+  title: "donut chart",
+  legend: "inline",
+  width: 350,
+  height: 350,
+  data: [
+    {
+      label: "water",
+      val: 5,
+      color: "#57d9ff"
+    },
+    {
+      label: "mobile",
+      val: 3,
+      color: "#f16e23"
+    },
+    {
+      label: "bulb",
+      val: 1,
+      color: "#ff00bf"
+    },
+    {
+      label: "wire",
+      val: 2,
+      color: "#937e88"
+    }
+  ]
+};
 
 export const ExampleComponent = ({ text }) => {
-  const options = {
-    title: "donut chart",
-    legend:"inline",
-    width: 350,
-    height: 350,
-    data: [
-      {
-        label: "water",
-        val: 5,
-        color: "#57d9ff"
-      },
-      {
-        label: "mobile",
-        val: 3,
-        color: "#f16e23"
-      },
-      {
-        label: "bulb",
-        val: 1,
-        color: "#ff00bf"
-      },
-      {
-        label: "wire",
-        val: 2,
-        color: "#937e88"
-      }
-    ]
+
+  const [option, setOption] = useState(options);
+
+  const onClick = () => {
+    const temp = JSON.parse(JSON.stringify(options));
+    temp.data[0].val = 30;
+    setOption(temp);
   }
+
   return <div className={styles.test}>
-    <h1>Example1</h1>
-    Example Component: {text}
+    <button onClick={onClick}>click to change data</button>
     <PieChart></PieChart>
-    <DonutChart options={options}></DonutChart>
+    <HalfDonutChart
+      options={option}
+    />
+    <DonutChart options={option}></DonutChart>
   </div>
 }
