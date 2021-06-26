0f3376154d9630c24fac09ec895156cf1f8b6b9f
CodeNinja47 <dinesh@reckonsys.com>
2021-06-26 11:38:45 +0530

    Fix

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { ExampleComponent } from 'reckonsys-charts'
+import { ReckonsysCharts } from 'reckonsys-charts'
 import 'reckonsys-charts/dist/index.css'
 import options from "./data"
 
 const App = () => {
-  return <ExampleComponent options={options}/>
+
+  const [option, setOption] = useState(options);
+
+  const onClick = () => {
+    const temp = JSON.parse(JSON.stringify(options));
+    temp.data[0].val = 30;
+    setOption(temp);
+  }
+
+  return <>
+    <button onClick={onClick}>click to change data</button>
+    <ReckonsysCharts options={option} />
+  </>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,55 +1,19 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styles from './styles.module.css'
 
 import PieChart from './components/Charts/PieChart';
 import DonutChart from './components/Charts/DonutChart';
 import HalfDonutChart from './components/HalfDonutChart/HalfDonutChart';
 
-const options = {
-  title: "donut chart",
-  legend: "inline",
-  width: 350,
-  height: 350,
-  data: [
-    {
-      label: "water",
-      val: 5,
-      color: "#57d9ff"
-    },
-    {
-      label: "mobile",
-      val: 3,
-      color: "#f16e23"
-    },
-    {
-      label: "bulb",
-      val: 1,
-      color: "#ff00bf"
-    },
-    {
-      label: "wire",
-      val: 2,
-      color: "#937e88"
-    }
-  ]
-};
-
-export const ExampleComponent = ({ text }) => {
-
-  const [option, setOption] = useState(options);
-
-  const onClick = () => {
-    const temp = JSON.parse(JSON.stringify(options));
-    temp.data[0].val = 30;
-    setOption(temp);
-  }
+export const ReckonsysCharts = ({ options }) => {
 
   return <div className={styles.test}>
-    <button onClick={onClick}>click to change data</button>
     <PieChart></PieChart>
     <HalfDonutChart
-      options={option}
+      options={options}
     />
-    <DonutChart options={option}></DonutChart>
+    <DonutChart
+      options={options}
+    />
   </div>
 }
